cc59e897915384d6ea2234b3c48685d383478c7c
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-07-31 17:01:14 -0700

    Complete fourth javascript-excersize

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arr, ...removeValues) {
+
+    const newArr = arr.filter(value => !removeValues.includes(value));
+    
+    return newArr;
 };
 
 // Do not edit below this line
