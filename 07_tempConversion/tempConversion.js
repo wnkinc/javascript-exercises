fc1d10c6a05e112842741db3fc29bc4d84d04b92
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-07-31 19:27:14 -0700

    Complete seventh javascript-excersize

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (5/9) * (fahrenheit - 32);
+  const roundedCelsius = parseFloat(celsius.toFixed(1));
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = ((9/5) * celsius) + 32;
+  const roundedFahrenheit = parseFloat(fahrenheit.toFixed(1));
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
