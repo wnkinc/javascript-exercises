213a1e50602efe1be68f5232662148c03d490ad3
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-07-31 19:13:26 -0700

    Complete sixth javascript-excersize

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYear = false;
+    if(year % 4 === 0){
+        leapYear = true;
+        if(year % 100 === 0) {
+            leapYear = false;
+            if(year % 400 === 0) leapYear = true;
+        }
+    }
+    return leapYear;
 };
 
 // Do not edit below this line
