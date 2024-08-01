5ab6bd4ec33b48801b276b89e5bd93f45d70a599
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-07-31 17:22:53 -0700

    Complete fifth javascript-excersize

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    let temp = 0;
 
+    if(startNum > endNum){
+        temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
+
+    if(startNum < 0 || !Number.isInteger(startNum) || !Number.isFinite(endNum)){
+        return 'ERROR';
+    }
+
+    for(let i = startNum; i <= endNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
