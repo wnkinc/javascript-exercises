9042bf0fb3bc31937ba761c9a93ff63354a2fec6
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-04 13:59:09 -0700

    Complete eigth javascript-excersize

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if(num1 === 0 || num1 === 1) return 1;
+  return num1 * factorial(num1-1);
 };
 
 // Do not edit below this line
