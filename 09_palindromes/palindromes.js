26f626afb22e245400a1990062b113844c592a45
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-04 14:16:28 -0700

    Complete ninth javascript-excersize

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let onlyChars = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+   let reversed = onlyChars.split('').reverse().join('');
+   if(reversed === onlyChars) return true;
+   else return false;
 };
 
 // Do not edit below this line
