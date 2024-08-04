4ac5c0ae9f1d3399717277a73b6416136fb57e83
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-04 16:55:30 -0700

    Complete twelfth javascript-excersize

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const year = new Date().getFullYear();
+    array.sort((a, b) => {
+        if(a.yearOfDeath === undefined) a.yearOfDeath = year;
+        if(b.yearOfDeath === undefined) b.yearOfDeath = year;
+       return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+    });
+    return array[array.length-1];
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+];
+
+console.log(findTheOldest(people));
+// array.reduce((accumulator, currentValue, currentIndex) => {
+//     let age = currentValue.yearOfBirth - currentValue.yearOfDeath
+//     if()
+
+//     accumulator = oldest;
+//  }, {});
 
 // Do not edit below this line
 module.exports = findTheOldest;
