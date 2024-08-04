977d4cccf5610690cc5a2392e596afe3723a8ff3
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-04 16:22:41 -0700

    Complete tenth javascript-excersize

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
 
+const fibonacci = function(number) {  
+    if(typeof number !== 'number') number = Number(number);
+    if(number === 0) return 0;
+    if(number < 0) return 'OOPS';
+    
+    let order = new Array(number);
+    order[0] = 1;
+    order[1] = 1;
+    for(let i = 2; i < number; i++){
+        order[i] = order[i-2] + order[i-1];
+    }
+    
+    return order[order.length-1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
